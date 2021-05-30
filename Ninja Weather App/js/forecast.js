7f5fe38e9590c1d44db445bfe34f3fbfcbfbbaa9
Sagar_b_Gourimath <sagar_b_gourimath@dell.com>
2021-05-30 21:51:32 +0530

    fetching weather info from accuweather api

--- Ninja Weather App/js/forecast.js
+++ Ninja Weather App/js/forecast.js
@@ -1,1 +1,30 @@
 const key = 'UvUb5Qv15YHLjGWGPu3i3Gsfz9RT5COH';
+
+const getWeatherInformation = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+const getCity = async (city) => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity('manchester')
+  .then((data) => {
+    return getWeatherInformation(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
