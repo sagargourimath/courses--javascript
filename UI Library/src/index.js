f60f263a5d58a25d448d937dc2dad8979e5d3285
Sagar Gourimath <sagar.gourimath@gmail.com>
2021-06-23 23:39:21 +0530

    dropdowns.

--- UI Library/src/index.js
+++ UI Library/src/index.js
@@ -1,4 +1,12 @@
 import Tooltip from './ninja-ui/tooltip';
+import Dropdown from './ninja-ui/dropdown';
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach((dropdown) => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
