3e450136556e084c8ca1e166cf82cea2806a39b9
Sagar Gourimath <sagar.gourimath@gmail.com>
2021-06-25 00:12:38 +0530

    tabs.

--- UI Library/src/index.js
+++ UI Library/src/index.js
@@ -1,5 +1,6 @@
 import Tooltip from './ninja-ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
+import Tabs from './ninja-ui/tabs';
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
@@ -10,3 +11,6 @@ dropdowns.forEach((dropdown) => {
   const instance = new Dropdown(dropdown);
   instance.init();
 });
+
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
