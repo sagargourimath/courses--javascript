63a561ed2acb9eee67e6c202daa25fc9e1f4f1d9
Sagar Gourimath <sagar.gourimath@gmail.com>
2021-06-25 00:23:34 +0530

    snackbar.

--- UI Library/src/index.js
+++ UI Library/src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from './ninja-ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
 import Tabs from './ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
@@ -14,3 +15,11 @@ dropdowns.forEach((dropdown) => {
 
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.init();
+
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  snackbar.show('You clicked me!');
+});
